43300b659a265aab5719d36436054cb11c5d1f8b
jaschers <Jann.Aschersleben@rwth-aachen.de>
2023-07-27 16:58:03 +0200

    clean up and add more plotting

--- scripts/calculate_flux.py
+++ scripts/calculate_flux.py
@@ -18,20 +18,29 @@ import astropy.units as u
 from tqdm import tqdm
 import multiprocessing as mp
 from functools import partial
+from matplotlib.colors import LinearSegmentedColormap
+import matplotlib.pyplot as plt
 
-def fill_table(table, flux_calculator, m_dm, sigma_v, args):
-    table["sigma_v [cm3 s-1]"] = sigma_v.value
-    r_cut = flux_calculator.radius_cut(m_dm, sigma_v)
-    flux = flux_calculator.gamma_flux(m_dm, args.channel, args.E_th, sigma_v)
-    table["r_cut [pc]"] = r_cut.to(u.pc).value
-    table["flux [cm-2 s-1]"] = flux.to(1 / (u.cm**2 * u.s)).value
-    return(table)
+plt.rcParams.update({'font.size': 8}) # 8 (paper), 10 (poster)
+plt.rc('text', usetex=True)
+plt.rc('font', family='Times New Roman')#, weight='normal', size=14)
+plt.rcParams['mathtext.fontset'] = 'cm'
+cm_conversion_factor = 1/2.54  # centimeters in inches
+single_column_fig_size = (8.85679 * cm_conversion_factor, 8.85679 * 3/4 * cm_conversion_factor)
 
-def extract_flux(bh_catalogue, flux_calculator, args, m_dm):
+
+def extract_flux_catalogue(bh_catalogue, flux_calculator, args, m_dm):
     flux_catalogue = pd.DataFrame()
     for sigma_v in args.sigma_v:
         table = bh_catalogue.copy()[["galaxy_id", "bh_id"]]
-        table = fill_table(table, flux_calculator, m_dm, sigma_v, args)
+
+        r_cut = flux_calculator.radius_cut(m_dm, sigma_v)
+        flux = flux_calculator.gamma_flux(m_dm, args.channel, args.E_th, sigma_v)
+        
+        table["sigma_v [cm3 s-1]"] = sigma_v.value
+        table["r_cut [pc]"] = r_cut.to(u.pc).value
+        table["flux [cm-2 s-1]"] = flux.to(1 / (u.cm**2 * u.s)).value
+        
         flux_catalogue = pd.concat([flux_catalogue, table], ignore_index = True)
     flux_catalogue.to_hdf(path + f"m_dm_{int(np.rint(m_dm.value))}GeV.h5", key = "table", mode = "w")
 
@@ -53,48 +62,23 @@ if __name__ == "__main__":
     print("Number of black holes:", len(bh_catalogue))
     print("Number of dark matter masses:", len(args.m_dm))
     print("Number of cross sections:", len(args.sigma_v))
-    # create flux_catalogue based on DM mass and cross section input
-    # if m_dm is an array and sigma_v is a single value, loop over m_dm and save flux_catalogue to a file for each m_dm
-    if len(args.m_dm) > 1 and len(args.sigma_v) == 1:
-        for m_dm in tqdm(args.m_dm):
-            flux_catalogue = bh_catalogue.copy()[["galaxy_id", "bh_id"]]
-            flux_catalogue = fill_table(flux_catalogue, flux_calculator, m_dm, args.sigma_v[0], args)
-            flux_catalogue.to_hdf(path + f"m_dm_{int(np.rint(m_dm.value))}GeV.h5", key = "table", mode = "w")
 
-    # if sigma_v is an array and m_dm is a single value, loop over sigma_v and save flux_catalogue to a single file
-    elif len(args.m_dm) == 1 and len(args.sigma_v) > 1:
-        flux_catalogue = pd.DataFrame()
-        for sigma_v in tqdm(args.sigma_v):
-            table = bh_catalogue.copy()[["galaxy_id", "bh_id"]]
-            table = fill_table(table, flux_calculator, args.m_dm[0], sigma_v, args)
-            flux_catalogue = pd.concat([flux_catalogue, table], ignore_index = True)
-        flux_catalogue.to_hdf(path + f"m_dm_{int(np.rint(args.m_dm[0].value))}GeV.h5", key = "table", mode = "w")
-
-    # if both m_dm and sigma_v are arrays, save flux_catalogue to a file for each m_dm
-    elif len(args.m_dm) > 1 and len(args.sigma_v) > 1:
-        # Set the number of processes to the number of available CPU cores or adjust as needed
-        num_processes = mp.cpu_count()
-        print("Start multiprocessing...")
-        print("Number of CPU cores available:", num_processes)
-        # if both m_dm and sigma_v are arrays, use multiprocessing to speed up the calculation
-        with mp.Pool(processes=num_processes) as pool:
-            # add necesarry arguments to the function except of DM masses since this is the variable to loop over
-            extract_flux_with_args = partial(
-                extract_flux, 
-                bh_catalogue,
-                flux_calculator, 
-                args
-                )
-            # Use tqdm to visualize the progress of the loop
-            # loop over all individual DM masses to calculate fluxes and save them to a file
-            for _ in tqdm(pool.imap_unordered(extract_flux_with_args, args.m_dm), total=len(args.m_dm)):
-                pass
-
-    # if both m_dm and sigma_v are single values, save flux_catalogue to a single file
-    else:
-        flux_catalogue = bh_catalogue.copy()[["galaxy_id", "bh_id"]]
-        table = fill_table(flux_catalogue, flux_calculator, args.m_dm[0], args.sigma_v[0], args)
-        flux_catalogue.to_hdf(path + f"m_dm_{int(np.rint(args.m_dm[0].value))}GeV.h5", key = "table", mode = "w")
+    num_processes = mp.cpu_count()
+    print("Start multiprocessing...")
+    print("Number of CPU cores available:", num_processes)
+    # if both m_dm and sigma_v are arrays, use multiprocessing to speed up the calculation
+    with mp.Pool(processes=num_processes) as pool:
+        # add necesarry arguments to the function except of DM masses since this is the variable to loop over
+        extract_flux_catalogue_with_args = partial(
+            extract_flux_catalogue, 
+            bh_catalogue,
+            flux_calculator, 
+            args
+            )
+        # Use tqdm to visualize the progress of the loop
+        # loop over all individual DM masses to calculate fluxes and save them to a file
+        for _ in tqdm(pool.imap_unordered(extract_flux_catalogue_with_args, args.m_dm), total=len(args.m_dm)):
+            pass
 
     print("Finished calculating fluxes!")
     print(f"Flux catalogue(s) saved to: {path}")
@@ -105,12 +89,39 @@ if __name__ == "__main__":
             path_plots = f"plots/{args.sim_name}/flux/{args.channel}_channel/"
             os.makedirs(path_plots, exist_ok = True)
 
-            if len(args.m_dm) > 1:
-                pass
-            else:
-                # get fluxes for different DM masses
+            cmap = LinearSegmentedColormap.from_list("", ["#fca311", "#e83151", "#003049"])
+            indices = np.linspace(0, 1, len(args.m_dm))
+            colors = cmap(indices)
+
+            # open relevant flux catalogues
+            flux_catalogues = []
+            for m_dm in args.m_dm:
+                flux_catalogues.append(pd.read_hdf(path + f"m_dm_{int(np.rint(m_dm.value))}GeV.h5", key = "table"))
+            
+            flux_catalogues_conat = pd.concat(flux_catalogues, ignore_index = True)
+            flux_plotter = dammplot.FluxPlotter(flux_catalogues_conat)
+            flux = flux_catalogues_conat["flux [cm-2 s-1]"].values * u.Unit("cm-2 s-1")
+            flux_th = flux_plotter.flux_thresholds(flux)
+
+            plt.figure(figsize = single_column_fig_size)
+            for flux_catalogue, m_dm, color in zip(flux_catalogues, args.m_dm, colors):
                 flux_plotter = dammplot.FluxPlotter(flux_catalogue)
-                flux_plotter.plot_integrated_luminosity(args.m_dm, args.sigma_v, args.E_th, path_plots + "integrated_luminosity.pdf")
+                flux_plotter.plot_integrated_luminosity(flux_th, m_dm, color)
+            plt.xlabel(f"$\Phi (E > {int(np.rint(args.E_th.value))}$ GeV) [cm$^{{-2}}$ s$^{{-1}}$]")
+            plt.ylabel(r"$N_{{\mathrm{BH}}}(>\Phi)$")
+            plt.xscale("log")
+            plt.yscale("log")
+            ymin, ymax = plt.ylim()
+            plt.ylim(ymin = 1, ymax = ymax)
+            plt.legend()
+            plt.tight_layout()
+            plt.savefig(path_plots + "integrated_luminosity.pdf", dpi = 300)
+            plt.close()
+
+
+            print(flux_catalogue)
+            # plot r_cut distribution for highest DM mass
+            flux_plotter.plot_cuttoff_radius_dist(path_plots + "r_cut_dist.pdf")
 
             print("Finished plotting!")
             print(f"Plots saved to: {path}plots/")
