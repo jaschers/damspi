c2b3d040caf6e40c870b7e58e2f8e46f5456f25c
jaschers <Jann.Aschersleben@rwth-aachen.de>
2023-12-21 15:43:21 +0100

    Commented out unused code and updated plot file names

--- scripts/plot_catalogue.py
+++ scripts/plot_catalogue.py
@@ -43,18 +43,18 @@ if __name__ == '__main__':
 
     # Plotting BH distributions
     print("Plotting BH distributions...")
-    bh_plotter.plot_dist_total(path)
+    # bh_plotter.plot_dist_total(path)
     bh_plotter.plot_dist_total_mean(path)
 
-    bh_plotter.plot_cumulative_radial_distribution(d_gc, path + "cumulative_radial_distribution.pdf")
+    # bh_plotter.plot_cumulative_radial_distribution(d_gc, path + "cumulative_radial_distribution.pdf")
     bh_plotter.plot_cumulative_radial_distribution_mean(path + "cumulative_radial_distribution_mean.pdf")
 
     # Plotting BH 2D maps
     print("Plotting BH 2D maps...")
-    bh_plotter.plot_2d_map(lat_gc, long_gc, path + "2d_map_gc.pdf")
+    # bh_plotter.plot_2d_map(lat_gc, long_gc, path + "2d_map_gc.pdf")
     bh_plotter.plot_2d_map(lat_sun, long_sun, path + "2d_map_sun.pdf")
 
-    bh_plotter.plot_2d_map_contours(lat_gc, long_gc, path + "2d_map_gc_contours.pdf")
+    # bh_plotter.plot_2d_map_contours(lat_gc, long_gc, path + "2d_map_gc_contours.pdf")
     bh_plotter.plot_2d_map_contours(lat_sun, long_sun, path + "2d_map_sun_contours.pdf")
 
     # Plot a 2D gaussian pdf of the BH distribution
