5e6488ad824946178e7192ba94b6ec673ac5095c
Jann Aschersleben <Jann.Aschersleben@rwth-aachen.de>
2024-01-09 16:55:43 +0100

    Improve code readability and maintainability

--- damspi/flux.py
+++ damspi/flux.py
@@ -5,6 +5,44 @@ from astropy.cosmology import Planck18 as cosmo
 from gammapy.astro.darkmatter import PrimaryFlux
 
 class FluxCalculator:
+    """
+    Class to calculate the gamma flux from DM annihilation around IMBHs
+
+    Parameters
+    ----------
+    bh_catalogue : pandas.DataFrame
+        BH catalogue with the following columns:
+        - z_f: final redshift
+        - r_sp [pc]: spacial radius
+        - gamma_sp: spacial slope
+        - rho(r_sp) [GeV/cm3]: spacial density
+        - d_Sun [kpc]: distance to the Sun
+        - m [M_solar]: BH mass
+    
+    Attributes
+    ----------
+    z_f : numpy.ndarray
+        formation redshift of the IMBHs
+    t_0 : numpy.ndarray
+        age of the universe
+    t_f : numpy.ndarray
+        formation time of the IMBHs
+    r_sp : numpy.ndarray
+        spike radius
+    gamma_sp : numpy.ndarray
+        spike index
+    rho_r_sp : numpy.ndarray
+        density at spike radius
+    distance : numpy.ndarray
+        distance to the Sun
+    M_bh : numpy.ndarray
+        BH mass
+    r_schw : numpy.ndarray
+        Schwarzschild radius
+    r_cut : numpy.ndarray
+        cut-off radius
+    """
+
     def __init__(self, bh_catalogue):
         self.bh_catalogue = bh_catalogue
         self.z_f = self.bh_catalogue['z_f'].values
@@ -19,22 +57,92 @@ class FluxCalculator:
 
     @staticmethod
     def redshift_to_time(z):
+        """
+        Convert redshift to time
+        
+        Parameters
+        ----------
+        z : float
+            redshift
+
+        Returns
+        -------
+        time : astropy.units.quantity.Quantity
+            time
+        """
+
         # Convert redshift to time
         time = cosmo.age(z).to(u.Gyr)
         return time
 
     @staticmethod
     def radius_lim(m_dm, sigma_v, time_0, time_f, r_sp, rho_r_sp, gamma_sp):
+        """
+        Calculate limitation radius used to calculate the cut-off radius
+
+        Parameters
+        ----------
+        m_dm : astropy.units.quantity.Quantity
+            DM mass
+        sigma_v : astropy.units.quantity.Quantity
+            velocity weighted DM cross section
+        time_0 : astropy.units.quantity.Quantity
+            age of the universe
+        time_f : astropy.units.quantity.Quantity
+            formation time of the IMBHs
+        r_sp : astropy.units.quantity.Quantity
+            spike radius
+        rho_r_sp : astropy.units.quantity.Quantity
+            density at spike radius
+        gamma_sp : float
+            spike index
+
+        Returns
+        -------
+        r_lim : astropy.units.quantity.Quantity
+            limitation radius
+        """
+
         rho_lim = (m_dm / (sigma_v * (time_0 - time_f))).to(u.GeV / u.cm**3)
         r_lim = (r_sp * (rho_lim / rho_r_sp)**(- 1 / gamma_sp)).to(u.pc)
         return(r_lim)
 
     @staticmethod
     def radius_schw(M_bh):
+        """
+        Calculate Schwarzschild radius
+
+        Parameters
+        ----------
+        M_bh : astropy.units.quantity.Quantity
+            BH mass
+
+        Returns
+        -------
+        r_schw : astropy.units.quantity.Quantity
+            Schwarzschild radius
+        """
+
         r_schw = (2 * const.G * M_bh / const.c**2).to(u.pc)
         return(r_schw)
 
     def radius_cut(self, m_dm, sigma_v):
+        """
+        Calculate cut-off radius
+
+        Parameters
+        ----------
+        m_dm : astropy.units.quantity.Quantity
+            DM mass
+        sigma_v : astropy.units.quantity.Quantity
+            velocity weighted DM cross section
+
+        Returns
+        -------
+        r_cut : astropy.units.quantity.Quantity
+            cut-off radius
+        """
+
         r_schw = self.radius_schw(self.M_bh)
         r_lim = self.radius_lim(m_dm, sigma_v, self.t_0, self.t_f, self.r_sp, self.rho_r_sp, self.gamma_sp)
         r_cut = np.maximum(4*r_schw.to(u.pc).value, r_lim.to(u.pc).value)
@@ -43,6 +151,24 @@ class FluxCalculator:
 
     @staticmethod
     def N_gamma(m_dm, channel, E_th):
+        """
+        Calculate number of gamma photons from DM annihilation
+
+        Parameters
+        ----------
+        m_dm : astropy.units.quantity.Quantity
+            DM mass
+        channel : str
+            annihilation channel
+        E_th : astropy.units.quantity.Quantity
+            threshold energy
+
+        Returns
+        -------
+        N : astropy.units.quantity.Quantity
+            number of gamma photons from DM annihilation
+        """
+
         # get number of gamma photons from DM annihilation with DM masses specified
         flux_annihi = PrimaryFlux(mDM = m_dm, channel = channel)
 
@@ -52,40 +178,41 @@ class FluxCalculator:
         N = flux_annihi.table_model.integral(E_th, m_dm).to(u.dimensionless_unscaled)
         return(N)
 
-    # def gamma_flux(self, m_dm, channel, E_th, sigma_v):
-    #     N = self.N_gamma(m_dm, channel, E_th)
-
-    #     # reference: https://journals.aps.org/prd/pdf/10.1103/PhysRevD.72.103517?casa_token=-e4eEEaCw5oAAAAA%3A9KIbORPLYWRlSVC5MyI3HSIslOLws15IjLMCUkoM2E3uD9PaUV_cXtfBta2anEGB9Epsa-J9DZ9qUiI
-    #     y = N * (self.rho_r_sp**2 * sigma_v * self.r_sp**3) / ((4 * self.gamma_sp - 6) * m_dm**2 * self.distance**2) * (self.r_cut / self.r_sp)**(-2 * self.gamma_sp + 3)
-    #     y = y.to(1 / (u.cm**2 * u.s))
-
-    #     return(y)
-
     def gamma_flux(self, m_dm, channel, E_th, sigma_v):
+        """
+        Calculate gamma flux from DM annihilation around IMBHs
+
+        Parameters
+        ----------
+        m_dm : astropy.units.quantity.Quantity
+            DM mass
+        channel : str
+            annihilation channel
+        E_th : astropy.units.quantity.Quantity
+            threshold energy
+        sigma_v : astropy.units.quantity.Quantity
+            velocity weighted DM cross section
+
+        Returns
+        -------
+        y : astropy.units.quantity.Quantity
+            gamma-ray flux from DM annihilation around IMBHs
+        """
+
         N = self.N_gamma(m_dm, channel, E_th)
 
         # prefactor
         alpha = 0.5 * sigma_v * N / (m_dm**2 * self.distance**2)
-        # term for 3r_schw < r < r_cut
-        # y1 = alpha * 0.5 * self.rho_r_sp**2 * (self.r_cut / self.r_sp)**(-2*self.gamma_sp) * self.r_cut * (self.r_cut**2 - 9 * self.r_schw**2)
-        # y1 = 0.5 * alpha * self.rho_r_sp**2 * (self.r_cut / self.r_sp)**(-2*self.gamma_sp) * self.r_cut * (self.r_cut**2 - 9 * self.r_schw**2)
-        y1 = 0.5 * alpha * self.rho_r_sp**2 * self.r_cut * (self.r_sp / self.r_cut)**(2*self.gamma_sp) * (self.r_cut**2 - 9 * self.r_schw**2)
-        # y1 = 1/3 * alpha * self.rho_r_sp**2 * (self.r_cut / self.r_sp)**(-2 * self.gamma_sp) * (self.r_cut**3 - 64 * self.r_schw**3)
-        # print("y1", np.unique(y1.to(1 / (u.cm**2 * u.s))))
+
+        # term for 2r_schw < r < r_cut
+        y1 = 0.5 * alpha * self.rho_r_sp**2 * self.r_cut * (self.r_sp / self.r_cut)**(2*self.gamma_sp) * (self.r_cut**2 - 4 * self.r_schw**2)
 
         # term for r_cut < r < r_sp
         # reference: https://journals.aps.org/prd/pdf/10.1103/PhysRevD.72.103517?casa_token=-e4eEEaCw5oAAAAA%3A9KIbORPLYWRlSVC5MyI3HSIslOLws15IjLMCUkoM2E3uD9PaUV_cXtfBta2anEGB9Epsa-J9DZ9qUiI
-        # y2 = N * (self.rho_r_sp**2 * sigma_v * self.r_sp**3) / ((4 * self.gamma_sp - 6) * m_dm**2 * self.distance**2) * (self.r_cut / self.r_sp)**(-2 * self.gamma_sp + 3)
         y2 = 2 * alpha * self.rho_r_sp**2 * self.r_sp**3 / (4*self.gamma_sp - 6) * (self.r_cut / self.r_sp)**(-2*self.gamma_sp + 3)
 
         y = y1 + y2
-        # y = y2
-
-        # print("y1/y2", np.unique(y1.to(1 / (u.cm**2 * u.s)) / y2.to(1 / (u.cm**2 * u.s))))
-
-        # exit()
 
         y = y.to(1 / (u.cm**2 * u.s))
 
         return(y)
-
--- damspi/utils.py
+++ damspi/utils.py
@@ -6,18 +6,49 @@ import pandas as pd
 import astropy.constants as const 
 
 def check_core_index_range(value):
+    """
+    Checks if core index is in range [0, 1)
+
+    Parameters
+    ----------
+    value : float
+        Core index
+
+    Returns
+    -------
+    fvlaue : float
+        Core index
+    """
     fvalue = float(value)
     if fvalue < 0 or fvalue >= 1:
         raise argparse.ArgumentTypeError(f"{value} is out of range [0, 1)")
     return fvalue
 
 def add_catalogue_args(parser):
+    """
+    Adds relevant IMBH calalogue arguments to the parser in the parse_args function
+
+    Parameters
+    ----------
+    parser : argparse.ArgumentParser
+        Argument parser
+    """
+
     parser.add_argument("-dmp", "--dark_matter_profile", type = str, required = False, default = "nfw", metavar = "-", choices = ["nfw", "cored"], help = "Dark matter density profile. Can be: 'nfw', 'cored'. Default: nfw")
     parser.add_argument("-ci", "--core_index", required = False, default = None, metavar = "-", type = check_core_index_range, help = "Cored dark matter density profile core index. Needs to fullfill: 0 <= core_index < 1. If None, the core index is a free paramter. Otherwise, the core index is fixed to specified value. Only has an effect if --dark_matter_profile='cored'. Default: None")
     parser.add_argument("-sa", "--save_animation", type = str, required = False, default = "n", metavar = "-", help = "Bool if animations for individual galaxies are saved (takes a lot of time) [y, n], default: n")
     parser.add_argument("-ltc", "--load_temporary_catalogue", type = str, required = False, default = "n", metavar = "-", help = "Yes, if temporary catalogue extracted in a previous run should be loaded. Only possible if create_catalogue.py was already run with the same -n <name> option in the past. Can be: 'y' or 'n'. Default: 'n'")
 
 def add_dark_matter_args(parser):
+    """
+    Adds relevant dark matter arguments to the parser in the parse_args function
+
+    Parameters
+    ----------
+    parser : argparse.ArgumentParser
+        Argument parser
+    """
+
     parser.add_argument("-mdm", "--m_dm", type = float, required = False, nargs = "+", default = [500, 1500, 3], metavar = "-", help = "Mass of dark matter particle in GeV. Can be single input or mass range + number of masses (three inputs). If mass range is given, scaling can be specified by the mass_dm_scaling argument. Default: 500 1500 3")
     parser.add_argument("-mdms", "--m_dm_scaling", type = str, required = False, default = "linear", metavar = "-", help = "Scaling of dark matter particle mass. Can be linear or log. Default: linear")
     parser.add_argument("-sv", "--sigma_v", type = float, required = False, nargs = "+", default = [3e-26], metavar = "-", help = "Dark matter (velocity weighted) annihilation cross section in cm3/s. Default: 3e-26")
@@ -27,15 +58,64 @@ def add_dark_matter_args(parser):
     parser.add_argument("-ic", "--instrument_comparison", type = str, required = False, default = "hess", choices = ["hess", "fermi"], metavar = "-", help = "Instrument to which the results are compared to. Default: hess")
 
 def add_plot_args(parser):
+    """
+    Adds relevant plot arguments to the parser in the parse_args function
+
+    Parameters
+    ----------
+    parser : argparse.ArgumentParser
+        Argument parser
+    """
+
     parser.add_argument("-plt", "--plot", type = str, required = False, default = "n", metavar = "-", help = "Bool if plots are saved [y, n], default: n")
 
 def add_labels_args(parser):
+    """
+    Adds relevant label arguments to the parser in the parse_args function
+    
+    Parameters
+    ----------
+    parser : argparse.ArgumentParser
+        Argument parser
+    """
+
     parser.add_argument("-l", "--labels", type = str, nargs = "+", default = ["NFW", r"free $\gamma_\mathrm{c}$", r"$\gamma_\mathrm{c} = 0.4$", r"$\gamma_\mathrm{c} = 0.0$"], metavar = "-", help = "Labels used in the plot legend.")
 
 def add_name_args(parser):
+    """
+    Adds relevant name arguments to the parser in the parse_args function
+
+    Parameters
+    ----------
+    parser : argparse.ArgumentParser
+        Argument parser
+    """
+
     parser.add_argument("-n", "--name", type = str, required = True,  nargs = "+", metavar = "-", help = "Suffix of the output filenames, e.g. catalogue_<name>.csv.")
 
 def parse_args(include_name = True, include_cat = False, include_dm = False, include_plot = False, include_labels = False):
+    """
+    Parses the arguments of the command line
+
+    Parameters
+    ----------
+    include_name : bool, optional
+        Bool if name arguments should be included, by default True
+    include_cat : bool, optional
+        Bool if catalogue arguments should be included, by default False
+    include_dm : bool, optional
+        Bool if dark matter arguments should be included, by default False
+    include_plot : bool, optional
+        Bool if plot arguments should be included, by default False
+    include_labels : bool, optional
+        Bool if label arguments should be included, by default False
+
+    Returns
+    -------
+    args : argparse.Namespace
+        Namespace containing all arguments
+    """
+
     script_descr="""
     Extracts IMBH catalogue from EAGLE data
     """
@@ -99,12 +179,42 @@ def parse_args(include_name = True, include_cat = False, include_dm = False, inc
     return args
 
 def convert_float_to_three_digit(number):
-        three_digit_number = '{:06d}'.format(int(number * 1000))
-        half_length = len(three_digit_number) // 2
-        first_half = str(three_digit_number[:half_length])
-        return(first_half)
+    """
+    Converts a float to a three digit number
+
+    Parameters
+    ----------
+    number : float
+        Float to be converted
+
+    Returns
+    -------
+    three_digit_number : str
+        Three digit number
+    """
+
+    three_digit_number = '{:06d}'.format(int(number * 1000))
+    half_length = len(three_digit_number) // 2
+    first_half = str(three_digit_number[:half_length])
+    return(first_half)
 
 def convert_float_to_six_digit(number):
+    """
+    Converts a float to a six digit number
+
+    Parameters
+    ----------
+    number : float
+        Float to be converted
+
+    Returns
+    -------
+    first_half : str
+        First half of the six digit number
+    second_half : str
+        Second half of the six digit number
+    """
+
     six_digit_number = '{:06d}'.format(int(np.round(number * 1000)))
     half_length = len(six_digit_number) // 2
     first_half = str(six_digit_number[:half_length])
@@ -112,11 +222,39 @@ def convert_float_to_six_digit(number):
     return(first_half, second_half)
 
 def redshift(a):
+    """
+    Calculates the redshift from the scale factor
+
+    Parameters
+    ----------
+    a : float
+        Scale factor
+
+    Returns
+    -------
+    z : float
+        Redshift
+    """
+
     z = (1 - a) / a
     z[z == np.inf] = 0
     return(z)
 
 def convert_to_bool(string):
+    """
+    Converts a string to a bool
+
+    Parameters
+    ----------
+    string : str
+        String to be converted
+
+    Returns
+    -------
+    bool
+        Bool
+    """
+
     if string == 'y':
         return(True)
     elif string == 'n':
@@ -125,15 +263,65 @@ def convert_to_bool(string):
         raise ValueError('Input must be either y or n')
 
 def nfw_profile(params, r):
+    """
+    NFW density profile
+
+    Parameters
+    ----------
+    params : tuple
+        Tuple of parameters (rho_0, r_s)
+    r : float
+        Distance to the centre of the halo
+
+    Returns
+    -------
+    rho : float
+        Density
+    """
+
     rho_0, r_s = params
     rho = (rho_0 * (r/r_s)**(-1) * (1+r/r_s)**(-2)).to(u.Msun/u.kpc**3)
     return(rho)
 
 def nfw_integral(r, rho_0, r_s):
+    """
+    NFW density profile integral
+
+    Parameters
+    ----------
+    r : float
+        Distance to the centre of the halo
+    rho_0 : float
+        Density at the scale radius
+    r_s : float
+        Scale radius
+
+    Returns
+    -------
+    y : float
+        Integral of the NFW density profile
+    """
+
     y = (4 * np.pi * rho_0 * r_s**3 * (np.log((r_s + r) / r_s) - r / (r_s + r))).to(u.Msun)
     return(y)
 
 def cored_profile(params, r):
+    """
+    Cored density profile
+
+    Parameters
+    ----------
+    params : tuple
+        Tuple of parameters (rho_0, r_s, r_c, gamma_c)
+    r : float
+        Distance to the centre of the halo
+
+    Returns
+    -------
+    rho : float
+        Cored density profile
+    """
+
     rho_0, r_s, r_c, gamma_c = params
     rho_nfw = nfw_profile((rho_0, r_s), r)
     rho_nfw_rc = nfw_profile((rho_0, r_s), r_c)
@@ -143,6 +331,28 @@ def cored_profile(params, r):
     return(rho)
 
 def cored_integral(r, rho_0, r_s, r_c, gamma_c):
+    """
+    Cored density profile integral
+
+    Parameters
+    ----------
+    r : float
+        Distance to the centre of the halo
+    rho_0 : float
+        Density at the scale radius
+    r_s : float
+        Scale radius
+    r_c : float
+        Core radius
+    gamma_c : float
+        Core index
+
+    Returns
+    -------
+    y : float
+        Integral of the cored density profile
+    """
+
     # integral if r < r_c
     y_cored = 4 * np.pi * nfw_profile((rho_0, r_s), r_c) * r_c**(gamma_c) * r**(3 - gamma_c) / (3 - gamma_c)
     # integral if r > r_c
@@ -154,14 +364,62 @@ def cored_integral(r, rho_0, r_s, r_c, gamma_c):
     return(y_total)
 
 def M_bh_2(M_bh):
+    """
+    Twice the black hole mass
+
+    Parameters
+    ----------
+    M_bh : float
+        Black hole mass
+
+    Returns
+    -------
+    y : float
+        Twice the black hole mass
+    """
+
     y = (2 * M_bh).to(u.Msun)
     return(y)
 
 def rescaled_distance(r, m200):
+    """
+    Rescales the distance to the centre of the halo
+
+    Parameters
+    ----------
+    r : float
+        Distance to the centre of the halo
+    m200 : float
+        M200 of the halo
+
+    Returns
+    -------
+    r : float
+        Rescaled distance to the centre of the halo
+    """
+
     r = r * (1e12 * u.Msun / m200)**(1/3)
     return(r)
 
 def remove_distant_satellites(table_bh, nsnap, args):
+    """
+    Removes satellites that are too far away from the host galaxy
+
+    Parameters
+    ----------
+    table_bh : pandas.DataFrame
+        Table containing the black hole data
+    nsnap : int
+        Snapshot number
+    args : argparse.Namespace
+        Namespace containing all arguments
+
+    Returns
+    -------
+    table_bh : pandas.DataFrame
+        Table containing the black hole data
+    """
+
     group_number = table_bh['group number'].values[0]
     subgroup_numbers = tuple(np.unique(table_bh['subgroup number'].values))
 
@@ -219,6 +477,26 @@ def remove_distant_satellites(table_bh, nsnap, args):
         return(table_bh)
     
 def parameter_distr_mean(table, parameter, bins):
+    """
+    Calculates the mean of the parameter distribution
+
+    Parameters
+    ----------
+    table : pandas.DataFrame
+        Table containing the black hole data
+    parameter : str
+        Parameter to be analysed
+    bins : int
+        Number of bins
+
+    Returns
+    -------
+    hist_mean : numpy.ndarray
+        Mean of the parameter distribution
+    hist_mean_error : numpy.ndarray
+        Error of the mean of the parameter distribution
+    """
+
     hist_list = []
     table_parameter = table[parameter]
     for galaxy_id in np.unique(table["galaxy_id"].values):
@@ -233,14 +511,60 @@ def parameter_distr_mean(table, parameter, bins):
     return hist_mean, hist_mean_error
 
 def gamma_core(gamma_sp):
+    """
+    Calculates the core index of the cored dark matter density profile
+
+    Parameters
+    ----------
+    gamma_sp : float
+        Spike index
+
+    Returns
+    -------
+    gamma_core : float
+        Core index
+    """
+
     gamma_core = (9-4*gamma_sp)/(2-gamma_sp)
     return(gamma_core)
 
 def spike_profile(params, r):
+    """
+    Spike density profile
+
+    Parameters
+    ----------
+    params : tuple
+        Tuple of parameters (rho_0, r_s, r_sp, gamma_sp)
+    r : float
+        Distance to the centre of the black hole
+
+    Returns
+    -------
+    rho : float
+        Spike density profile
+    """
+
     rho_0, r_s, r_sp, gamma_sp = params
     return nfw_profile((rho_0, r_s), r_sp) * (r/r_sp)**(-gamma_sp)
 
 def imbh_profile(params, r):
+    """
+    IMBH density profile
+
+    Parameters
+    ----------
+    params : tuple
+        Tuple of parameters (rho_0, r_schw, r_cut, r_s, r_sp, gamma_sp)
+    r : float
+        Distance to the centre of the black hole
+
+    Returns
+    -------
+    rho : float
+        IMBH density profile
+    """
+
     rho_0, r_schw, r_cut, r_s, r_sp, gamma_sp = params
 
     if r <= 2 * r_schw:
