c4e7876836ad4b092bc800018502a93ca7777bea
Jann Aschersleben <Jann.Aschersleben@rwth-aachen.de>
2024-01-15 13:19:46 +0100

    Refactor plot_catalogue.py

--- scripts/plot_catalogue.py
+++ scripts/plot_catalogue.py
@@ -37,27 +37,29 @@ if __name__ == '__main__':
     path = f"plots/{args.sim_name}/black_hole_dist/{args.name}/"
     os.makedirs(path, exist_ok = True)
 
-    # # Plotting BH number distributions
-    print("Plotting BH number distributions...")
-    bh_plotter.plot_number_dist(path)
+    # # # Plotting BH number distributions
+    # print("Plotting BH number distributions...")
+    # bh_plotter.plot_number_dist(path)
 
-    # Plotting BH distributions
-    print("Plotting BH distributions...")
-    # bh_plotter.plot_dist_total(path)
-    bh_plotter.plot_dist_total_mean(path)
+    # # Plotting BH distributions
+    # print("Plotting BH distributions...")
+    # # bh_plotter.plot_dist_total(path)
+    # bh_plotter.plot_dist_total_mean(path)
 
-    # bh_plotter.plot_cumulative_radial_distribution(d_gc, path + "cumulative_radial_distribution.pdf")
-    bh_plotter.plot_cumulative_radial_distribution_mean(path + "cumulative_radial_distribution_mean.pdf")
+    # # bh_plotter.plot_cumulative_radial_distribution(d_gc, path + "cumulative_radial_distribution.pdf")
+    # bh_plotter.plot_cumulative_radial_distribution_mean(path + "cumulative_radial_distribution_mean.pdf")
 
-    # Plotting BH 2D maps
-    print("Plotting BH 2D maps...")
-    # bh_plotter.plot_2d_map(lat_gc, long_gc, path + "2d_map_gc.pdf")
-    bh_plotter.plot_2d_map(lat_sun, long_sun, path + "2d_map_sun.pdf")
+    # # Plotting BH 2D maps
+    # print("Plotting BH 2D maps...")
+    # # bh_plotter.plot_2d_map(lat_gc, long_gc, path + "2d_map_gc.pdf")
+    # bh_plotter.plot_2d_map(lat_sun, long_sun, path + "2d_map_sun.pdf")
 
     # bh_plotter.plot_2d_map_contours(lat_gc, long_gc, path + "2d_map_gc_contours.pdf")
-    bh_plotter.plot_2d_map_contours(lat_sun, long_sun, path + "2d_map_sun_contours.pdf")
+    bh_plotter.plot_2d_map_contours(lat_sun, long_sun, path + "2d_map_sun_contours.pdf", upsampling = True, upsampling_factor = 1)
 
-    # Plot a 2D gaussian pdf of the BH distribution
-    bh_plotter.plot_2d_map_gaussian(lat_sun, long_sun, path + "2d_map_sun_gaussian.pdf")
+    # bh_plotter.plot_healpix_map(lat_sun, long_sun, path + "2d_healpix_map_sun.pdf")
 
-    print(f"Plots saved in {path}")
+    # # Plot a 2D gaussian pdf of the BH distribution
+    # bh_plotter.plot_2d_map_gaussian(lat_sun, long_sun, path + "2d_map_sun_gaussian.pdf")
+
+    # print(f"Plots saved in {path}")
